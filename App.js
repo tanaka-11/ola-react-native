810604a158995b6e3d7d69a7e0978a1cc96baab0
Marina Tanaka <mtanakasg@gmail.com>
2024-02-07 09:25:40 -0300

    Componente básico

--- App.js
+++ App.js
@@ -1,21 +1,12 @@
-import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View } from "react-native";
+import { StatusBar, Text, View } from "react-native";
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text style={{ fontSize: 30 }}>@_Tanaka11_</Text>
-      <Text style={{ fontSize: 22 }}>Olá Mundo - React Native 🥶</Text>
-      <StatusBar style="auto" />
-    </View>
+    <>
+      <StatusBar barStyle={"default"} />
+      <View>
+        <Text>@_Tanaka11_</Text>
+      </View>
+    </>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
-  },
-});
